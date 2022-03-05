37eb700ffbce9e6c8f680ca6a84f18296b6ed261
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-05 02:16:17 -0500

    Added a better windows 95 logo

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,6 +1,8 @@
 import './NavBar.css'
 import Clock from 'react-live-clock';
 import Start from '../Start/Start';
+import { FiGithub } from 'react-icons/fi'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 export default function NavBar() {
   return (
@@ -9,7 +11,9 @@ export default function NavBar() {
             <Start />
         </div>
         <div className='Clock'>
-            <Clock />
+            <a href='https://github.com/Jacobdye99' target="_blank" rel="noreferrer" className='NavLinks' ><FiGithub size={20}/></a>
+            <a href="https://www.linkedin.com/in/jacob-dye-55659b149/" target="_blank" rel="noreferrer" className='NavLinks'><AiOutlineLinkedin size={20}/></a>
+            <Clock className={"Clocktext"}/>
         </div>
     </div>
   )
--- client/src/components/Start/Start.jsx
+++ client/src/components/Start/Start.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import './Start.css'
 
 export default function Start() {
   return (
-    <div><img src='Assets/windows-472132.png' />Start</div>
+    <div className='StartDiv'>
+        <img className="winImg" src='https://github.com/Jacobdye99/Portfolio/blob/dev/client/Assets/windows-472132.png?raw=true' alt='windows 95' />
+        <h3 className='StartText'>Start</h3>
+    </div>
   )
 }
