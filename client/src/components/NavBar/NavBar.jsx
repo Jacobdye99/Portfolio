e94f032848ca681a211f1b2836729f266408f27f
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-05 01:17:57 -0500

    Fixing image for nav

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,13 @@
 import './NavBar.css'
 import Clock from 'react-live-clock';
+import Start from '../Start/Start';
 
 export default function NavBar() {
   return (
     <div className='Nav'>
+        <div className='Start'>
+            <Start />
+        </div>
         <div className='Clock'>
             <Clock />
         </div>
---
+++ client/src/components/Start/Start.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Start() {
+  return (
+    <div><img src='Assets/windows-472132.png' />Start</div>
+  )
+}
