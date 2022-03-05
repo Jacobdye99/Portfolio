59d544d024a7653c4feca5496da35027cee7ed21
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-05 03:35:39 -0500

    StartMenu lookin great

--- client/src/components/StartMenu/StartMenu.jsx
+++ client/src/components/StartMenu/StartMenu.jsx
@@ -5,7 +5,10 @@ import { Link } from 'react-router-dom'
 export default function StartMenu() {
   return (
     <div className='StartMenuDiv'>
-        <Link to="/" className='StartLinks'>Logout</Link>
+        <Link to="/" className='StartLinks'>
+            <img src="https://github.com/Jacobdye99/Portfolio/blob/dev/client/src/Assets/Images/artage-io-48177_1646468426.png?raw=true" alt="Windows 95 computer logo" className='StartImg'/>
+            <h4 className='StartText2'>Logout</h4>
+        </Link>
     </div>
   )
 }
