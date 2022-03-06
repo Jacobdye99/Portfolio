accf2942670afe06462a0f4284dc34c4b0d23b93
Jacob Dye <jacobdye@pop-os.localdomain>
2022-03-05 23:36:19 -0500

    Added some more icons

---
+++ client/src/components/Desktop/Desktop.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export default function Desktop(props) {
+  return (
+    <div>
+        <div>
+            Resume
+        </div>
+        <div>
+            Projects
+        </div>
+        <div>
+            Skills
+        </div>
+    </div>
+  )
+}
